fa658238d49d69650fb2544b5fe06a7edb5f255f
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2026-07-28 12:13:19 -0400

    fix: corregir conflicto entre ufo y statuscol que ocultaba números de línea

--- lua/plugins/spec/ufo.lua
+++ lua/plugins/spec/ufo.lua
@@ -3,7 +3,6 @@ return {
   event = "BufReadPost",
   dependencies = { "kevinhwang91/promise-async" },
   init = function()
-    vim.o.foldcolumn = "1"
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
