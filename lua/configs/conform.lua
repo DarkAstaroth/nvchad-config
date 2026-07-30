a8fa35a4742904cc402c847795f66cd1affbe4d0
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2026-07-30 09:29:47 -0400

    feat: usar biome automáticamente en proyectos con biome.json, prettier como fallback

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,23 @@
+local has_biome = function(self, ctx)
+  return vim.fs.find({ "biome.json", "biome.jsonc" }, { path = ctx.filename, upward = true })[1] ~= nil
+end
+
 local options = {
+  formatters = {
+    biome = {
+      condition = has_biome,
+      require_cwd = true,
+    },
+  },
+
   formatters_by_ft = {
     lua        = { "stylua" },
-    javascript = { "eslint", "prettier" },
-    typescript = { "eslint", "prettier" },
+    javascript = { "biome", "eslint", "prettier", stop_after_first = true },
+    typescript = { "biome", "eslint", "prettier", stop_after_first = true },
+    json       = { "biome", "prettier",            stop_after_first = true },
     vue        = { "eslint", "prettier" },
     css        = { "prettier" },
     html       = { "prettier" },
-    json       = { "prettier" },
     markdown   = { "prettier" },
     yaml       = { "prettier" },
   },
